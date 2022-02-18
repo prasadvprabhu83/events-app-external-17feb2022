a67e38b2104e28e30758cc7889c9c116e251fcbf
Prasad Prabhu <99856205+prasadvprabhu83@users.noreply.github.com>
2022-02-18 16:00:35 +0530

    Updated Test for Title Change
    Updated Test for Title Change

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to Cloud Contenders' Team1New application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to Cloud Contenders' Final application</h1>"));
         return done();
       });
 
